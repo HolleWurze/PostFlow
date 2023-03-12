5c0f69fb6c61a8729387da029539f04252061878
Sergey Sviridenko <aveflash@gmail.com>
2023-03-12 15:16:56 +0100

    Fix home page

--- src/main/java/project/pet/PostFlow/PostFlowApplication.java
+++ src/main/java/project/pet/PostFlow/PostFlowApplication.java
@@ -3,7 +3,7 @@ package project.pet.PostFlow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "project.pet.PostFlow")
+@SpringBootApplication
 public class PostFlowApplication {
 
 	public static void main(String[] args) {
